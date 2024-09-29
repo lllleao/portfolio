e01b97b700d4625375ad76b71f630554a558686e
leao <lucasleaolima@gmail.com>
2024-09-29 20:58:04 -0300

    new cv

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -7,7 +7,7 @@ import Slider from '@components/Slider'
 import * as S from './styles'
 
 import avatar from '@images/perfil-fundo2.png'
-import cv from '@images/Currículo.pdf'
+import cv from '@images/CV.pdf'
 import { useInView } from 'react-intersection-observer'
 
 const Home = () => {
